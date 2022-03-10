ac603c35be8652a43c27831effbee2c314de8c21
Sura102 <surafelbrhan1@gmail.com>
2022-03-10 05:24:02 -0800

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /*
  * main = main block
  * Return: 0
  */
 int main(void)
 {
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19")
 	return (0);
 }
